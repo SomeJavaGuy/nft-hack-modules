32330b5a36db3904362a535f9b5e48a143bc8db3
dphengsiaroun <dphengsiaroun@outlook.fr>
2021-03-21 18:47:11 +0100

    ajoute les package manquant

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 var index = require('./routes/index');
 var app = express();
+const port = 3000;
 
 require('dotenv').config()
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -32,5 +37,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var router = express.Router()
-const axios = require('axios')
 
 const pinataApiKey = process.env.PINATA_API_KEY
 const pinataSecretApiKey = process.env.SECRET_API_KEY
@@ -11,9 +10,9 @@ const FormData = require('form-data')
 router.post('pin', async (req, res, next) => {
   const url = private.env.PINATA_ROUTE
 
-  let res = new FormData()
+  let resp = new FormData()
 
-  res.append('file', fs.createReadStream('../public/images/image1.jpeg'))
+  resp.append('file', fs.createReadStream('../public/images/image1.jpeg'))
 
   const data = await axios
     .post(url, data, {
@@ -25,27 +24,29 @@ router.post('pin', async (req, res, next) => {
       },
     })
     .catch(console.log)
-  
+
   res.send(data.data)
 })
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send({ title: 'Express' })
+  res.send({
+    title: 'Express'
+  })
 })
 
 //Generate wallet ETH
 router.post('/generate', async function (req, res, next) {
   const data = await axios
     .post(
-      'https://api-sandbox.circle.com/v1/wallets/1000080700/addresses',
-      {
+      'https://api-sandbox.circle.com/v1/wallets/1000080700/addresses', {
         idempotencyKey: '2678c583-20d1-405d-b185-047f8d01fe6c',
         currency: 'USD',
         chain: 'ETH',
-      },
-      {
-        headers: { Authorization: 'Bearer ' + process.env.API_KEY },
+      }, {
+        headers: {
+          Authorization: 'Bearer ' + process.env.API_KEY
+        },
       }
     )
     .catch(console.log)
@@ -59,9 +60,10 @@ router.post('/generate', async function (req, res, next) {
 router.post('/status', async function (req, res, next) {
   const data = await axios
     .post(
-      'https://api-sandbox.circle.com/v1/payments/81855279-b53d-4119-9f1e-5d0af00f0c2',
-      {
-        headers: { Authorization: 'Bearer ' + process.env.API_KEY },
+      'https://api-sandbox.circle.com/v1/payments/81855279-b53d-4119-9f1e-5d0af00f0c2', {
+        headers: {
+          Authorization: 'Bearer ' + process.env.API_KEY
+        },
       }
     )
     .catch(console.log)
@@ -71,20 +73,30 @@ router.post('/status', async function (req, res, next) {
 
 
 router.post('/payment', function (req, res, next) {
-  res.send({ title: 'Express' })
+  res.send({
+    title: 'Express'
+  })
 })
 
 router.post('/payout', async function (req, res, next) {
   const data = await axios.post(
-    'https://api-sandbox.circle.com/v1/payouts',
-    {
+    'https://api-sandbox.circle.com/v1/payouts', {
       idempotencyKey: 'ff9d668a-ccde-48c3-82fd-8deb2a6f7c28',
-      destination: { type: 'wire', id: '66686a41-1403-49b5-b124-9652b3210bf2' },
-      amount: { currency: 'USD', amount: '1.00' },
-      metadata: { beneficiaryEmail: 'john.smith@email.com' },
-    },
-    {
-      headers: { Authorization: 'Bearer ' + process.env.API_KEY },
+      destination: {
+        type: 'wire',
+        id: '66686a41-1403-49b5-b124-9652b3210bf2'
+      },
+      amount: {
+        currency: 'USD',
+        amount: '1.00'
+      },
+      metadata: {
+        beneficiaryEmail: 'john.smith@email.com'
+      },
+    }, {
+      headers: {
+        Authorization: 'Bearer ' + process.env.API_KEY
+      },
     }
   )
 
@@ -95,18 +107,21 @@ router.post('/payout', async function (req, res, next) {
 router.post('/cPayout', async function (req, res, next) {
   console.log("LAB")
   const data = await axios.post(
-    'https://api-sandbox.circle.com/v1/businessAccount/transfers',
-    {
+    'https://api-sandbox.circle.com/v1/businessAccount/transfers', {
       idempotencyKey: '4ca72ac7-5217-49bb-bbe6-e8dcb4c53c25',
       destination: {
         type: 'verified_blockchain',
         addressId: '0x493A9869E3B5f846f72267ab19B76e9bf99d51b1',
         chain: 'ETH',
       },
-      amount: { amount: '0.10', currency: 'USD' },
-    },
-    {
-      headers: { Authorization: 'Bearer ' + process.env.API_KEY },
+      amount: {
+        amount: '0.10',
+        currency: 'USD'
+      },
+    }, {
+      headers: {
+        Authorization: 'Bearer ' + process.env.API_KEY
+      },
     }
   ).catch(console.log)
 
